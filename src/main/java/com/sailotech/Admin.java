e215414f4ad82c2eb3e78c93aa89f5d0fc87864f
supraja1996 <srilakshmi.supraja@sailotech.com>
2021-11-05 11:39:48 +0530

    First commit

---
+++ src/main/java/com/sailotech/Admin.java
@@ -0,0 +1,5 @@
+package com.sailotech;
+
+public class Admin {
+
+}
---
+++ src/test/java/com/sailotech/CreateDoctor.java
@@ -0,0 +1,23 @@
+package com.sailotech;
+
+import org.testng.annotations.Test;
+
+
+
+public class CreateDoctor {
+	
+	
+		
+		public CreateDoctor() {
+			super();
+			System.out.println("*** CreateDoctorTest instantiated ***");
+			// TODO Auto-generated constructor stub
+		}
+		
+		
+		@Test
+		public void test_login() {
+			System.out.println("***** login test success ****");
+		}
+
+}
